4d15e6fe975667b3fc1d95d15a71ee7dfa9db27b
Imtiyaz Allam <95128488+imtiyazallam07@users.noreply.github.com>
2026-06-27 08:19:28 +0530

    v0.0.3

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,5 +5,5 @@ import firebaseConfig from "../../firebase-applet-config.json";
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-export const db = getFirestore(app);
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const auth = getAuth(app);
